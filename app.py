6872610c6e3ede077198b3ce9fe7f8aa5b840091
lufiasfarza <55400820+lufiasfarza@users.noreply.github.com>
2025-08-05 02:47:35 +0800

    Fix dynamic port for Railway deploy

--- app.py
+++ app.py
@@ -149,4 +149,4 @@ if __name__ == '__main__':
     print("  GET  /api/v1/health - Health check")
     print("")
     
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
